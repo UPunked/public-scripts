68048581531ae97a37551ab34430e879aba847b9
PUNKED LSRP <punkedlsrp@gmail.com>
2023-03-25 21:24:18 +0000

    forgot to add fxmanifest update for server, done now.

--- server-data/resources/[syn]/syn_river/fxmanifest.lua
+++ server-data/resources/[syn]/syn_river/fxmanifest.lua
@@ -2,6 +2,10 @@ fx_version "adamant"
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 game "rdr3"
 
+server_scripts { --forgot to add
+	'server.lua',
+}
+
 client_scripts { 
 	'rio_beber.lua',
 }
